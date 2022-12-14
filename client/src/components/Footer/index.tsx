395093d53cce126bb7f8fb0597ef801d3047dec6
Heitor <heitor.cardoso39@gmail.com>
2022-12-14 16:22:36 -0300

    feat: footer and logo components

---
+++ client/src/components/Footer/index.tsx
@@ -0,0 +1,21 @@
+import Logo from '../Logo'
+import { FooterContainer } from './style'
+import { FaAngleUp } from 'react-icons/fa'
+
+function Footer() {
+	const scrollToTop = () => {
+		window.scrollTo({ top: 0 })
+	}
+
+	return (
+		<FooterContainer>
+			<Logo white />
+			<p>© 2022 - Todos os direitos reservados.</p>
+			<button onClick={scrollToTop}>
+				<FaAngleUp size="1.5em" />
+			</button>
+		</FooterContainer>
+	)
+}
+
+export default Footer
