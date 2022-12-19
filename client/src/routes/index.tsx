28f610ed36af1bb6cd5d1cc277aef73ecb91a126
Heitor <heitor.cardoso39@gmail.com>
2022-12-19 02:28:11 -0300

    fix: new example routes login, register, dashboard

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,11 +1,29 @@
 import { createBrowserRouter } from 'react-router-dom'
+import { Dashboard } from '../pages/dashBoard'
 import { PageError } from '../pages/errorPage'
 import { Home } from '../pages/homePage'
+import { Login } from '../pages/loginPage'
+import { Register } from '../pages/registerPage'
 
 export const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <Home />,
+		errorElement: <PageError />,
+	},
+	{
+		path: '/login',
+		element: <Login />,
+		errorElement: <PageError />,
+	},
+	{
+		path: '/register',
+		element: <Register />,
+		errorElement: <PageError />,
+	},
+	{
+		path: '/dashboard',
+		element: <Dashboard />,
 		errorElement: <PageError />,
 	},
 ])
