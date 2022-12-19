3ffa6ef7d7b9e480390cd49aac189ff2509b0108
matheuszeiser <matheuszaizer@hotmail.com>
2022-12-19 10:23:30 -0300

    Updating with develop

---
+++ client/src/components/CardProfile/style.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+interface ColorRandomProps {
+    color: string;
+}
+
+export const ThemeCardProfile = styled.div<ColorRandomProps>`
+    display: flex;
+    align-items: center;
+    margin-top: 10px;
+    margin-bottom: 10px;
+
+    .initial {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        width: 32px;
+        height: 32px;
+
+        background-color: var(--${({color}) => color});
+
+        padding: 16px 7.5px;
+        margin-right: 8px;
+
+        border-radius: 50%;
+
+        color: var(--brand4);
+
+        font-size: 0.875rem;
+        font-weight: 500;
+    }
+`;
