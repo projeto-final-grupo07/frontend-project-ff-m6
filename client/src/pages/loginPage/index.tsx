ee7d4774424702f2348bb51f7dfd373b1c412e3c
Heitor <heitor.cardoso39@gmail.com>
2022-12-19 02:29:35 -0300

    feat: new page for example login

---
+++ client/src/pages/loginPage/index.tsx
@@ -0,0 +1,17 @@
+import Footer from '../../components/Footer'
+import Navbar from '../../components/Navbar'
+import { StyledParagraph, StyledTitle } from '../../styles/typography'
+
+export const Login = () => {
+	return (
+		<>
+			<Navbar />
+
+			<StyledTitle fontSize="Heading-1-700" tag="h1">
+				Olá! essa e a pagina de Login
+			</StyledTitle>
+			<StyledParagraph>Bem-vindo!</StyledParagraph>
+			<Footer />
+		</>
+	)
+}
