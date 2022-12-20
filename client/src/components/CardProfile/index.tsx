a3f7c5d2dd7508386845ff4a3d9771ed6adb7d2f
matheuszeiser <matheuszaizer@hotmail.com>
2022-12-20 14:20:43 -0300

    feat: updated card profile initials

--- client/src/components/CardProfile/index.tsx
+++ client/src/components/CardProfile/index.tsx
@@ -6,12 +6,14 @@ interface CardProfileProps {
 }
 
 const CardProfile = ({ name }: CardProfileProps) => {
-  const initial = name.split('')[0].toUpperCase();
+  const arrName = name.split(' ');
+
+  const initials = arrName[0][0] + arrName[arrName.length - 1][0];
 
   const randomColor = 'random' + (1 + Math.floor(Math.random() * 12));
   return (
     <ThemeCardProfile color={randomColor}>
-      <span className='initial'>{initial}</span>
+      <span className='initial'>{initials}</span>
       <StyledTitle className='nameAuthor' fontSize='body-2-500' tag='p'>
         {name}
       </StyledTitle>
