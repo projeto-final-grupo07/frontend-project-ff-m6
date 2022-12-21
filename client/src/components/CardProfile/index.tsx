97bd42ede3cfb7941bf22941d69e241d1bf610ef
Heitor <heitor.cardoso39@gmail.com>
2022-12-20 23:15:52 -0300

    fix: now can ajust font size in card profile'

--- client/src/components/CardProfile/index.tsx
+++ client/src/components/CardProfile/index.tsx
@@ -3,14 +3,15 @@ import { ThemeCardProfile } from './style';
 
 interface CardProfileProps {
   name: string;
+  size?: string;
 }
 
-const CardProfile = ({ name }: CardProfileProps) => {
+const CardProfile = ({ name, size }: CardProfileProps) => {
   const initial = name.split('')[0].toUpperCase();
 
   const randomColor = 'random' + (1 + Math.floor(Math.random() * 12));
   return (
-    <ThemeCardProfile color={randomColor}>
+    <ThemeCardProfile color={randomColor} size={size || '32px'}>
       <span className='initial'>{initial}</span>
       <StyledTitle className='nameAuthor' fontSize='body-2-500' tag='p'>
         {name}
