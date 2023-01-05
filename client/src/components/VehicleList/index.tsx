b10534cc8e09b664abbd2df09715c44807cfd852
matheuszeiser <matheuszaizer@hotmail.com>
2023-01-05 10:46:20 -0300

    feat: list of vehicles with axios

--- client/src/components/VehicleList/index.tsx
+++ client/src/components/VehicleList/index.tsx
@@ -1,4 +1,5 @@
-import { data } from '../../fakeData/data';
+import { useEffect, useState } from 'react';
+import api from '../../services';
 import ProductCard from '../ProductCard';
 import { ThemeCarList } from './style';
 
@@ -6,11 +7,52 @@ interface IVehicleListProps {
   typeVehicle: boolean;
 }
 
+interface IVehicle {
+  id: string;
+  typeOffer: boolean;
+  title: string;
+  year: number;
+  mileage: number;
+  price: number;
+  describe: string;
+  typeVehicles: boolean;
+  coverImg: string;
+  GalleryImg: IGalleryImg[];
+  user: IUser;
+}
+
+export interface IUser {
+  name: string;
+  email: string;
+  password: string;
+  cpf: string;
+  phone: string;
+  birthDate: string;
+  describe: string;
+  typeAccount: boolean;
+  is_active: boolean;
+  id: string;
+}
+
+interface IGalleryImg {
+  id?: string;
+  url: string;
+}
+
 const VehicleList = ({ typeVehicle }: IVehicleListProps) => {
+  const [vehicles, setVehicles] = useState<IVehicle[]>([]);
+
+  useEffect(() => {
+    api
+      .get('/vehicle')
+      .then((resp) => setVehicles(resp.data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <ThemeCarList>
       <div className='divCars'>
-        {data.map((vehicle) => {
+        {vehicles?.map((vehicle) => {
           if (vehicle.typeVehicles == typeVehicle)
             return (
               <ProductCard
@@ -18,10 +60,10 @@ const VehicleList = ({ typeVehicle }: IVehicleListProps) => {
                 title={vehicle.title}
                 describe={vehicle.describe}
                 coverImg={vehicle.coverImg}
-                name={vehicle.name}
+                name={vehicle.user.name}
                 mileage={vehicle.mileage}
                 year={vehicle.year}
-                price={vehicle.price}
+                price={+vehicle.price}
               ></ProductCard>
             );
         })}
