317a557624e6fb4f6854695670be9e031c262027
fjrjdev <thedromemafia@gmail.com>
2023-01-09 18:51:42 -0300

    feat; added breakpoints size

--- client/src/components/StyledModal/ImgModal/index.tsx
+++ client/src/components/StyledModal/ImgModal/index.tsx
@@ -10,7 +10,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: '50%',
+  width: { xs: '100%', sm: '70%', md: '50%' },
   bgcolor: 'background.paper',
   boxShadow: 24,
   p: 4,
@@ -52,6 +52,7 @@ function ImgModal({ children, nameModal, altName, src }: ICreateModalProps) {
   return (
     <>
       <img alt={altName} src={src} onClick={() => setOpen(!open)} />
+
       <Modal
         open={open}
         onClose={handleClose}
