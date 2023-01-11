ff5a09ebfa20de97cd91414b33f352d70e6b543f
Heitor <heitor.cardoso39@gmail.com>
2023-01-11 00:10:47 -0300

    feat: error page

--- client/src/pages/errorPage/index.tsx
+++ client/src/pages/errorPage/index.tsx
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom';
+import { StyledButton } from '../../styles/button';
+import { Container } from './style';
+import { BiErrorCircle } from 'react-icons/bi';
+
 export const PageError = () => {
   return (
-    <>
-      <h1>Error</h1>
-    </>
+    <Container>
+      <div>
+        <BiErrorCircle size={'2rem'} className='icon' />
+        <h1>Algum erro ocorreu!</h1>
+        <p>Verifique se o URL está correto e tente novamente.</p>
+        <Link to={'/'}>
+          <StyledButton buttonStyle='grey1'>Volte a pagina inicial</StyledButton>
+        </Link>
+      </div>
+    </Container>
   );
 };
