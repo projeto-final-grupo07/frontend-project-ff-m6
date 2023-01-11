198028aac7a23cf748fba42fc9cf78a50ea9d112
fjrjdev <thedromemafia@gmail.com>
2023-01-11 20:32:59 -0300

    hotfix: vehicle detail user profile page error

--- client/src/pages/vehicleDetail/index.tsx
+++ client/src/pages/vehicleDetail/index.tsx
@@ -33,7 +33,6 @@ import { CommentContext } from '../../contexts/CommentContext/CommentContext';
 import ImgModal from '../../components/StyledModal/ImgModal';
 import StyledModal from '../../components/StyledModal';
 
-
 export const VehicleDetail = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -41,10 +40,8 @@ export const VehicleDetail = () => {
   const { id } = useParams();
 
   const { userData, loadUser } = useContext(UserContext);
-  const { commentData, setCommentData, setId, setVehicleData, setPrice, vehicleData, price, getVehicle } =
+  const { commentData, setCommentData, setVehicleData, setPrice, vehicleData, price } =
     useContext(CommentContext);
-
-  setId(id);
 
   const token = localStorage.getItem('token');
   const [loading, setLoading] = useState(false);
@@ -127,162 +124,167 @@ export const VehicleDetail = () => {
     <>
       <ModalEditComment />
       <StyledVehicleDetail>
-      <Navbar />
-      <div className='divRoxinha'></div>
-      <div className='mainContainer'>
-        <StyledSection>
-          <StyledBox className='mainImage'>
-            <img src={vehicleData?.coverImg} alt='Vehicle' />
-          </StyledBox>
-          <StyledBox>
-            <StyledDivGap>
-              <StyledTitle fontSize='Heading-6-600' tag='h6'>
-                {vehicleData?.title}
-              </StyledTitle>
-              <SytledDivInfoVehicle>
-                <div>
-                  <StyledTitle fontSize='body-2-500' tag='p' className='tag'>
-                    {vehicleData?.year}
-                  </StyledTitle>
-                  <StyledTitle fontSize='body-2-500' tag='p' className='tag'>
-                    {vehicleData?.mileage} KM
-                  </StyledTitle>
-                </div>
-                <StyledTitle fontSize='Heading-7-500' tag='h3'>
-                  {price.toLocaleString('pt-br', {
-                    style: 'currency',
-                    currency: 'BRL',
-                  })}
+        <Navbar />
+        <div className='divRoxinha'></div>
+        <div className='mainContainer'>
+          <StyledSection>
+            <StyledBox className='mainImage'>
+              <img src={vehicleData?.coverImg} alt='Vehicle' />
+            </StyledBox>
+            <StyledBox>
+              <StyledDivGap>
+                <StyledTitle fontSize='Heading-6-600' tag='h6'>
+                  {vehicleData?.title}
                 </StyledTitle>
-              </SytledDivInfoVehicle>
-            </StyledDivGap>
-            <StyledModal
-              propsButton={{
-                buttonStyle: 'brand',
-                buttonSize: 'medium',
-              }}
-              nameModal='Comprar'
-              nameButtonOpen='Comprar'
-            >
-              Chama no zap
-            </StyledModal>
-          </StyledBox>
-          <StyledBox>
-            <StyledDivGap>
-              <StyledTitle fontSize='Heading-6-600' tag='h6'>
-                Descrição
-              </StyledTitle>
-              <StyledTitle fontSize='body-1-400' tag='p' fontColor='var(--grey2)'>
-                {vehicleData?.describe}
-              </StyledTitle>
-            </StyledDivGap>
-          </StyledBox>
-          <StyledBox>
-            <StyledDivGap>
-              <StyledTitle fontSize='Heading-6-600' tag='h6'>
-                Comentários
-              </StyledTitle>
-              {commentData?.map((comment) => {
-                return (
-                  <CommentCard
-                    key={comment.id}
-                    name={comment.user?.name || 'Name'}
-                    comment={comment.comment}
-                    daysOfComment={days(new Date(comment.createdAt))}
-                    commentId={comment.id}
-                  />
-                );
-              })}
-            </StyledDivGap>
-          </StyledBox>
-          <StyledBox>
-            <StyledDivGap>
-              {token ? (
-                <CardProfile name={userData?.name || 'Name'} userId={userData?.id} />
-              ) : (
-                <></>
-              )}
-              <StyledForm
-                onSubmit={handleSubmit((evt) => {
-                  return onSubmitFunction(evt);
-                })}
+                <SytledDivInfoVehicle>
+                  <div>
+                    <StyledTitle fontSize='body-2-500' tag='p' className='tag'>
+                      {vehicleData?.year}
+                    </StyledTitle>
+                    <StyledTitle fontSize='body-2-500' tag='p' className='tag'>
+                      {vehicleData?.mileage} KM
+                    </StyledTitle>
+                  </div>
+                  <StyledTitle fontSize='Heading-7-500' tag='h3'>
+                    {price.toLocaleString('pt-br', {
+                      style: 'currency',
+                      currency: 'BRL',
+                    })}
+                  </StyledTitle>
+                </SytledDivInfoVehicle>
+              </StyledDivGap>
+              <StyledModal
+                propsButton={{
+                  buttonStyle: 'brand',
+                  buttonSize: 'medium',
+                }}
+                nameModal='Comprar'
+                nameButtonOpen='Comprar'
               >
-                <div className='textarea-container'>
-                  <StyledRegisterComment
-                    rows={2}
-                    maxLength={250}
-                    placeholder='Carro muito confortável, foi uma ótima experiência de compra...'
-                    {...register('message')}
-                  ></StyledRegisterComment>
-                  <StyledButton
-                    type='submit'
-                    className='commentButton'
-                    buttonStyle={errors?.message ? 'alert' : token ? 'brand' : 'disabled'}
-                    buttonSize='medium'
-                  >
-                    {loading ? <BiLoaderCircle size={'2em'} className='loading' /> : 'Comentar'}
-                  </StyledButton>
-                </div>
-              </StyledForm>
-            </StyledDivGap>
-          </StyledBox>
-        </StyledSection>
-        <StyledSectionFixed>
-          <StyledPhotoDetail>
-            <h2>Fotos</h2>
-            <Grid
-              container
-              justifyContent='center'
-              spacing={1}
-              columns={12}
-              className='gridGallery'
-            >
-              {vehicleData?.GalleryImgs.length === 0 ? (
-                <StyledBox className='noImage'>Sem mais imagens</StyledBox>
-              ) : (
-                vehicleData?.GalleryImgs.map((image) => {
+                Chama no zap
+              </StyledModal>
+            </StyledBox>
+            <StyledBox>
+              <StyledDivGap>
+                <StyledTitle fontSize='Heading-6-600' tag='h6'>
+                  Descrição
+                </StyledTitle>
+                <StyledTitle fontSize='body-1-400' tag='p' fontColor='var(--grey2)'>
+                  {vehicleData?.describe}
+                </StyledTitle>
+              </StyledDivGap>
+            </StyledBox>
+            <StyledBox>
+              <StyledDivGap>
+                <StyledTitle fontSize='Heading-6-600' tag='h6'>
+                  Comentários
+                </StyledTitle>
+                {commentData?.map((comment, index) => {
                   return (
-                    <Grid
-                      key={image?.id}
-                      item
-                      xs={4}
-                      sm={4}
-                      md={4}
-                      lg={4}
-                      sx={{ cursor: 'pointer' }}
-                    >
-                      <ImgModal
-                        key={image?.id}
-                        src={image?.url}
-                        altName={'Imagem de um veículo'}
-                        nameModal='Imagem do veículo'
-                        className='imgGallery'
-                      >
-                        <img src={image?.url} alt={'Imagem de um veículo'} className='imgGallery' />
-                      </ImgModal>
-                    </Grid>
+                    <CommentCard
+                      key={index}
+                      userId={comment.user_id}
+                      name={comment.user?.name || 'Name'}
+                      comment={comment.comment}
+                      daysOfComment={days(new Date(comment.createdAt))}
+                      commentId={comment.id}
+                    />
                   );
-                })
-              )}
-            </Grid>
-          </StyledPhotoDetail>
-          <StyledBox center flex columnFlex paddingC='30px'>
-            <StyledDivGap center>
-              <CardProfile direction='true' name={vehicleData?.user.name || 'undefined'} />
-              <StyledTitle fontSize='body-1-400' tag='p' fontColor='var(--grey2)'>
-                {vehicleData?.user.describe}
-              </StyledTitle>
-            </StyledDivGap>
-            <Link to={`/profile/${vehicleData?.user_id}`}>
-              <StyledButton buttonStyle='grey1' buttonSize='medium'>
-                Ver todos os anuncios
-              </StyledButton>
-            </Link>
-          </StyledBox>
-        </StyledSectionFixed>
-      </div>
-      <Footer />
-    </StyledVehicleDetail>
+                })}
+              </StyledDivGap>
+            </StyledBox>
+            <StyledBox>
+              <StyledDivGap>
+                {token ? (
+                  <CardProfile name={userData?.name || 'Name'} userId={userData?.id} />
+                ) : (
+                  <></>
+                )}
+                <StyledForm
+                  onSubmit={handleSubmit((evt) => {
+                    return onSubmitFunction(evt);
+                  })}
+                >
+                  <div className='textarea-container'>
+                    <StyledRegisterComment
+                      rows={2}
+                      maxLength={250}
+                      placeholder='Carro muito confortável, foi uma ótima experiência de compra...'
+                      {...register('message')}
+                    ></StyledRegisterComment>
+                    <StyledButton
+                      type='submit'
+                      className='commentButton'
+                      buttonStyle={errors?.message ? 'alert' : token ? 'brand' : 'disabled'}
+                      buttonSize='medium'
+                    >
+                      {loading ? <BiLoaderCircle size={'2em'} className='loading' /> : 'Comentar'}
+                    </StyledButton>
+                  </div>
+                </StyledForm>
+              </StyledDivGap>
+            </StyledBox>
+          </StyledSection>
+          <StyledSectionFixed>
+            <StyledPhotoDetail>
+              <h2>Fotos</h2>
+              <Grid
+                container
+                justifyContent='center'
+                spacing={1}
+                columns={12}
+                className='gridGallery'
+              >
+                {vehicleData?.GalleryImgs.length === 0 ? (
+                  <StyledBox className='noImage'>Sem mais imagens</StyledBox>
+                ) : (
+                  vehicleData?.GalleryImgs.map((image) => {
+                    return (
+                      <Grid
+                        key={image?.id}
+                        item
+                        xs={4}
+                        sm={4}
+                        md={4}
+                        lg={4}
+                        sx={{ cursor: 'pointer' }}
+                      >
+                        <ImgModal
+                          key={image?.id}
+                          src={image?.url}
+                          altName={'Imagem de um veículo'}
+                          nameModal='Imagem do veículo'
+                          className='imgGallery'
+                        >
+                          <img
+                            src={image?.url}
+                            alt={'Imagem de um veículo'}
+                            className='imgGallery'
+                          />
+                        </ImgModal>
+                      </Grid>
+                    );
+                  })
+                )}
+              </Grid>
+            </StyledPhotoDetail>
+            <StyledBox center flex columnFlex paddingC='30px'>
+              <StyledDivGap center>
+                <CardProfile direction='true' name={vehicleData?.user.name || 'undefined'} />
+                <StyledTitle fontSize='body-1-400' tag='p' fontColor='var(--grey2)'>
+                  {vehicleData?.user.describe}
+                </StyledTitle>
+              </StyledDivGap>
+              <Link to={`/profile/${vehicleData?.user_id}`}>
+                <StyledButton buttonStyle='grey1' buttonSize='medium'>
+                  Ver todos os anuncios
+                </StyledButton>
+              </Link>
+            </StyledBox>
+          </StyledSectionFixed>
+        </div>
+        <Footer />
+      </StyledVehicleDetail>
     </>
   );
 };
